5e6bf8b2cd625208d271ca7e506034c410ac8100
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-22 14:20:56 +0700

    menuliskan cout void output

--- IntoOOP/0079_IntoOOP.cpp
+++ IntoOOP/0079_IntoOOP.cpp
@@ -8,5 +8,7 @@ class Mahasiswa{
         string jurusan;
 
         void output(){
-            
+            cout << "Nama : " << nama << endl;
+            cout << "Umur : " << umur << endl;
+            cout << "Jurusan : " << jurusan << endl;
         }

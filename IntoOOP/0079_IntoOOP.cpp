2356e32475888a4ee7ce79ac932fe11f24db9da6
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-22 14:24:49 +0700

    menuliskan void input

--- IntoOOP/0079_IntoOOP.cpp
+++ IntoOOP/0079_IntoOOP.cpp
@@ -19,4 +19,17 @@ class Matakuliah{
         string KodeMk;
         string NamaMk;
         int sks;
+    
+    public:
+        void input() {
+            cout << "Kode MK : ";
+            cin >> KodeMk;
+            cout << "Nama MK : ";
+            cin >> NamaMk;
+            cout << "SKS : ";
+            cin >> sks;
+        }
+
+
+
 }
